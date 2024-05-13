cf374e7e4b69fe9d6da24e082cdb7e2dc998d15e
NhatMinh666 <nhatminh291297@gmail.com>
2024-05-13 13:51:00 +0700

    Test gitignore

--- EFCoreAssignmentDay1/EFCoreAssignmentDay1/obj/Debug/net8.0/EFCoreAssignmentDay1.AssemblyInfo.cs
+++ EFCoreAssignmentDay1/EFCoreAssignmentDay1/obj/Debug/net8.0/EFCoreAssignmentDay1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFCoreAssignmentDay1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c32bb931f47184efd7d90fc037348f64eca000b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFCoreAssignmentDay1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFCoreAssignmentDay1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
